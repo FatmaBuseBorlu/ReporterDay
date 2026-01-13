a4e858c4568c7f90e60318b40be8e37f42d2f814
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2026-01-13 09:16:18 +0300

    .

---
+++ ReporterDay.PresentationLayer/ViewComponents/ArticleDetailViewComponents/_ArticleDetailCommentsComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ReporterDay.PresentationLayer.ViewComponents.ArticleDetailViewComponents
+{
+    public class _ArticleDetailCommentsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+            
+        }
+    }
+}
