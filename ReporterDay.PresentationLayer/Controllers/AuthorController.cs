3924e670f6190ce70d87a1ea78d4b48c62aaf4b7
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2026-01-16 20:46:06 +0300

    .

--- ReporterDay.PresentationLayer/Controllers/AuthorController.cs
+++ ReporterDay.PresentationLayer/Controllers/AuthorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ReporterDay.BusinessLayer.Abstract;
 using ReporterDay.EntityLayer.Entities;
 
@@ -7,23 +8,47 @@ namespace ReporterDay.PresentationLayer.Controllers
 {
     public class AuthorController : Controller
     {
-        private readonly UserManager<AppUser> _userManager; 
+        private readonly UserManager<AppUser> _userManager;
         private readonly IArticleService _articleService;
-
-        public async Task <IActionResult> CreateArticle()
+        private readonly ICategoryService _categoryService;
+        public AuthorController(UserManager<AppUser> userManager, IArticleService articleService, ICategoryService categoryService)
         {
-            var user= await _userManager.FindByNameAsync(User.Identity.Name);
+            _userManager = userManager;
+            _articleService = articleService;
+            _categoryService = categoryService;
+        }
+        public async Task<IActionResult> CreateArticle()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.id = user.Id;
-            ViewBag.name = user.Name +" "+ user.Surname;
+            ViewBag.name = user.Name + " " + user.Surname;
+
+            List<SelectListItem> values = (from x in _categoryService.TGetListAll()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.categories = values;
+
             return View();
         }
-        
-          
-     
+
         [HttpPost]
         public async Task<IActionResult> CreateArticle(Article article)
         {
-            return RedirectToAction("Index","Default");
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            article.AppUserId = user.Id;
+            article.CreatedDate = DateTime.Now;
+            _articleService.TInsert(article);
+            return RedirectToAction("Index", "Default");
+        }
+
+        public async Task<IActionResult> MyArticleList()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var values = _articleService.TGetArticlesByAuthor(user.Id);
+            return View(values);
         }
     }
 }
