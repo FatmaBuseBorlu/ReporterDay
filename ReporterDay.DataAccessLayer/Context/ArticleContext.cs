4521cb96925cb1a2cb492df32e4a92f4b5e93e15
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2026-06-18 23:02:07 +0300

    Remove hardcoded SQL Server name from DbContext

--- ReporterDay.DataAccessLayer/Context/ArticleContext.cs
+++ ReporterDay.DataAccessLayer/Context/ArticleContext.cs
@@ -1,25 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ReporterDay.EntityLayer.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReporterDay.DataAccessLayer.Context
 {
     public class ArticleContext : IdentityDbContext<AppUser>
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-NBRMDOS;initial catalog=ReporterBlogDayDb;integrated security=true;trust server certificate=true");
         }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Slider> Sliders { get; set; }
         public DbSet<Tag> Tags { get; set; }
-
     }
 }
